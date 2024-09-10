52b9853ce85613540e0984d80baabc26f18e555b
ulrichsinn <uli@addtothenoise.com>
2024-09-09 18:27:19 -0700

    pubspec updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,218 @@
+import 'package:flow_builder/flow_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+
+part 'main.freezed.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flow Demo',
+      home: HomePage(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// MAIN PAGE
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends HookWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final userInfo = useState<UserInfo?>(null);
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+      backgroundColor:
+      userInfo.value == null ? Colors.white : userInfo.value!.favoriteColor,
+      appBar: AppBar(title: const Text('Flow')),
+      body: Container(
+        padding: const EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        child: userInfo.value == null
+            ? ElevatedButton(
+          onPressed: () async {
+            userInfo.value =
+            await Navigator.of(context).push(OnboardingFlow.route());
+          },
+          child: const Text('GET STARTED'),
+        )
+            : Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Welcome, ${userInfo.value!.name}!',
+              style: const TextStyle(fontSize: 48.0),
+            ),
+            const SizedBox(height: 48.0),
+            Text(
+              'So, you are ${userInfo.value!.age} years old and this is your favorite color? Great!',
+              style: const TextStyle(fontSize: 32.0),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+// FLOW
+
+class OnboardingFlow extends StatelessWidget {
+  const OnboardingFlow({super.key});
+
+  static Route<UserInfo> route() {
+    return MaterialPageRoute(builder: (_) => const OnboardingFlow());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FlowBuilder<UserInfo>(
+        state: const UserInfo(),
+        onGeneratePages: (profile, pages) {
+          return [
+            const MaterialPage(child: NameForm()),
+            if (profile.name != null) const MaterialPage(child: AgeForm()),
+            if (profile.age != null) const MaterialPage(child: ColorForm()),
+          ];
+        },
+      ),
+    );
+  }
+}
+
+// FORMS
+
+class NameForm extends HookWidget {
+  const NameForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final name = useState<String?>(null);
+    return Scaffold(
+      appBar: AppBar(title: const Text('Name')),
+      body: Container(
+        padding: const EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              autofocus: true,
+              onChanged: (value) => name.value = value,
+              decoration: const InputDecoration(
+                labelText: 'Name',
+                hintText: 'Enter your name',
+              ),
+            ),
+            const SizedBox(height: 24.0),
+            ElevatedButton(
+              child: const Text('Continue'),
+              onPressed: () {
+                if (name.value != null && name.value!.isNotEmpty) {
+                  context
+                      .flow<UserInfo>()
+                      .update((info) => info.copyWith(name: name.value));
+                }
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AgeForm extends HookWidget {
+  const AgeForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final age = useState<int?>(null);
+    return Scaffold(
+      appBar: AppBar(title: const Text('Age')),
+      body: Container(
+        padding: const EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            DropdownButtonFormField<int>(
+              items: List.generate(
+                200,
+                    (index) => DropdownMenuItem(
+                  value: index,
+                  child: Text(index.toString()),
+                ),
+              ),
+              onChanged: (value) => age.value = value,
+              decoration: const InputDecoration(
+                labelText: 'Age',
+                hintText: 'How old are you?',
+              ),
+            ),
+            const SizedBox(height: 24.0),
+            ElevatedButton(
+              child: const Text('Continue'),
+              onPressed: () {
+                if (age.value != null) {
+                  context
+                      .flow<UserInfo>()
+                      .update((info) => info.copyWith(age: age.value));
+                }
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ColorForm extends HookWidget {
+  const ColorForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final color = useState<Color>(Colors.amber);
+    return Scaffold(
+      appBar: AppBar(title: const Text('Favorite Color')),
+      body: Container(
+        padding: const EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ColorPicker(
+              pickerColor: color.value,
+              onColorChanged: (value) => color.value = value,
+              pickerAreaHeightPercent: 0.8,
+            ),
+            const SizedBox(height: 24.0),
+            ElevatedButton(
+              child: const Text('Continue'),
+              onPressed: () {
+                context.flow<UserInfo>().complete(
+                        (info) => info.copyWith(favoriteColor: color.value));
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// DOMAIN
+
+@freezed
+abstract class UserInfo with _$UserInfo {
+  const factory UserInfo({
+    String? name,
+    int? age,
+    Color? favoriteColor,
+  }) = _UserInfo;
 }
---
+++ lib/main.freezed.dart
@@ -0,0 +1,178 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'main.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$UserInfo {
+  String? get name => throw _privateConstructorUsedError;
+  int? get age => throw _privateConstructorUsedError;
+  Color? get favoriteColor => throw _privateConstructorUsedError;
+
+  /// Create a copy of UserInfo
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $UserInfoCopyWith<UserInfo> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $UserInfoCopyWith<$Res> {
+  factory $UserInfoCopyWith(UserInfo value, $Res Function(UserInfo) then) =
+      _$UserInfoCopyWithImpl<$Res, UserInfo>;
+  @useResult
+  $Res call({String? name, int? age, Color? favoriteColor});
+}
+
+/// @nodoc
+class _$UserInfoCopyWithImpl<$Res, $Val extends UserInfo>
+    implements $UserInfoCopyWith<$Res> {
+  _$UserInfoCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of UserInfo
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = freezed,
+    Object? age = freezed,
+    Object? favoriteColor = freezed,
+  }) {
+    return _then(_value.copyWith(
+      name: freezed == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String?,
+      age: freezed == age
+          ? _value.age
+          : age // ignore: cast_nullable_to_non_nullable
+              as int?,
+      favoriteColor: freezed == favoriteColor
+          ? _value.favoriteColor
+          : favoriteColor // ignore: cast_nullable_to_non_nullable
+              as Color?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$UserInfoImplCopyWith<$Res>
+    implements $UserInfoCopyWith<$Res> {
+  factory _$$UserInfoImplCopyWith(
+          _$UserInfoImpl value, $Res Function(_$UserInfoImpl) then) =
+      __$$UserInfoImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String? name, int? age, Color? favoriteColor});
+}
+
+/// @nodoc
+class __$$UserInfoImplCopyWithImpl<$Res>
+    extends _$UserInfoCopyWithImpl<$Res, _$UserInfoImpl>
+    implements _$$UserInfoImplCopyWith<$Res> {
+  __$$UserInfoImplCopyWithImpl(
+      _$UserInfoImpl _value, $Res Function(_$UserInfoImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of UserInfo
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = freezed,
+    Object? age = freezed,
+    Object? favoriteColor = freezed,
+  }) {
+    return _then(_$UserInfoImpl(
+      name: freezed == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String?,
+      age: freezed == age
+          ? _value.age
+          : age // ignore: cast_nullable_to_non_nullable
+              as int?,
+      favoriteColor: freezed == favoriteColor
+          ? _value.favoriteColor
+          : favoriteColor // ignore: cast_nullable_to_non_nullable
+              as Color?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$UserInfoImpl implements _UserInfo {
+  const _$UserInfoImpl({this.name, this.age, this.favoriteColor});
+
+  @override
+  final String? name;
+  @override
+  final int? age;
+  @override
+  final Color? favoriteColor;
+
+  @override
+  String toString() {
+    return 'UserInfo(name: $name, age: $age, favoriteColor: $favoriteColor)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$UserInfoImpl &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.age, age) || other.age == age) &&
+            (identical(other.favoriteColor, favoriteColor) ||
+                other.favoriteColor == favoriteColor));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, name, age, favoriteColor);
+
+  /// Create a copy of UserInfo
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$UserInfoImplCopyWith<_$UserInfoImpl> get copyWith =>
+      __$$UserInfoImplCopyWithImpl<_$UserInfoImpl>(this, _$identity);
+}
+
+abstract class _UserInfo implements UserInfo {
+  const factory _UserInfo(
+      {final String? name,
+      final int? age,
+      final Color? favoriteColor}) = _$UserInfoImpl;
+
+  @override
+  String? get name;
+  @override
+  int? get age;
+  @override
+  Color? get favoriteColor;
+
+  /// Create a copy of UserInfo
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$UserInfoImplCopyWith<_$UserInfoImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
